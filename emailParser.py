f89b3cd8d544859c910c6ee69d72791fad7b8371
Eric Hernandez <eric.hernandez@speedwellholdings.com>
2019-01-06 19:38:26 -0500

    started parsing emails

--- emailParser.py
+++ emailParser.py
@@ -6,6 +6,18 @@ class email_files:
     def get_list_of_files(pattern):
         return glob.glob(pattern)
 
+class parse_shadow_backup_emails:
 
-list_files = email_files.get_list_of_files("/tmp/save/email_*")
-print(list_files)
+    def __init__(self,list_of_email_files):
+        list_of_email_files = list_of_email_files
+
+    def get_subjects(self):
+        for emails in list_of_email_files:
+            print(emails)
+
+list_of_email_files = email_files.get_list_of_files("/tmp/save/email_*")
+
+parse_emails = parse_shadow_backup_emails(list_of_email_files)
+parse_emails.get_subjects()
+
+
