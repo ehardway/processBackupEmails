3f383c999e14553b6585478ae2345cad0a1915f9
Eric Hernandez <eric.hernandez@speedwellholdings.com>
2019-01-06 19:29:00 -0500

    Testing

--- emailParser.py
+++ emailParser.py
@@ -1,3 +1,11 @@
-from os import listdir
+import glob
 
-os.listdir("/tmp")
+class email_files:
+
+    @staticmethod
+    def get_list_of_files(pattern):
+        return glob.glob(pattern)
+
+
+list_files = email_files.get_list_of_files("/tmp/save/email_*")
+print(list_files)
