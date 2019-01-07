8afad525824300ab382ba1a1a41ca7273bd0e8b0
Eric Hernandez <eric.hernandez@speedwellholdings.com>
2019-01-06 20:46:14 -0500

    Creating a list of subjects from email files

--- emailParser.py
+++ emailParser.py
@@ -1,41 +1,37 @@
 import glob
 import re
 
+
 class email_files:
 
     @staticmethod
     def get_list_of_files(pattern):
         return glob.glob(pattern)
 
-class parse_shadow_backup_emails:
 
+class parse_shadow_backup_emails:
     subjects = []
 
-    def __init__(self,list_of_email_files):
-        list_of_email_files = list_of_email_files
+    def __init__(self, directory):
+        self.list_of_email_files = email_files.get_list_of_files(directory)
 
     def get_subjects(self):
-        for email in list_of_email_files:
+        for email in self.list_of_email_files:
             with open(email) as f:
                 file_data = f.readlines()
-                self.get_match_and_next_line("^Subject", file_data)
+                self.get_match_and_next_line("^Subject:", file_data)
             f.closed
+        return self.subjects
 
     def get_match_and_next_line(self, pattern, file_data):
-        self.pattern = pattern
-        self.file_data = file_data
         for line_number, line in enumerate(file_data):
-            if re.search(pattern, line, re.I):
+            if re.search(pattern, line):
                 subject = line.rstrip() + " " + file_data[line_number + 1].rstrip()
                 self.subjects.append(subject)
 
-    def show_all_subjects(self):
-        print(self.subjects)
 
-list_of_email_files = email_files.get_list_of_files("/tmp/save/email_*")
+parse_emails = parse_shadow_backup_emails("/tmp/save/*")
 
-parse_emails = parse_shadow_backup_emails(list_of_email_files)
-parse_emails.get_subjects()
-parse_emails.show_all_subjects()
+subjects = parse_emails.get_subjects()
 
-
+print(subjects)
