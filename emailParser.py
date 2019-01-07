79a46c424fbb02bf60f4d8e8e56885a1e9bb8b8a
Eric Hernandez <eric.hernandez@speedwellholdings.com>
2019-01-06 20:34:47 -0500

    Extracting all subjects

--- emailParser.py
+++ emailParser.py
@@ -1,4 +1,5 @@
 import glob
+import re
 
 class email_files:
 
@@ -16,16 +17,25 @@ class parse_shadow_backup_emails:
     def get_subjects(self):
         for email in list_of_email_files:
             with open(email) as f:
-                read_data = f.read()
-                print(read_data)
-                f.closed
+                file_data = f.readlines()
+                self.get_match_and_next_line("^Subject", file_data)
+            f.closed
 
-        def get_match_and_next_line(self,pattern):
-            self.pattern = pattern
+    def get_match_and_next_line(self, pattern, file_data):
+        self.pattern = pattern
+        self.file_data = file_data
+        for line_number, line in enumerate(file_data):
+            if re.search(pattern, line, re.I):
+                subject = line.rstrip() + " " + file_data[line_number + 1].rstrip()
+                self.subjects.append(subject)
+
+    def show_all_subjects(self):
+        print(self.subjects)
 
 list_of_email_files = email_files.get_list_of_files("/tmp/save/email_*")
 
 parse_emails = parse_shadow_backup_emails(list_of_email_files)
 parse_emails.get_subjects()
+parse_emails.show_all_subjects()
 
 
