5b43d14d1462c426e8cb65bc4bff5ce9cc1bc5a8
Eric Hernandez <eric.hardway@gmail.com>
2019-04-28 17:10:00 +0000

    changed email directory

--- postfix_hook.php
+++ postfix_hook.php
@@ -5,7 +5,7 @@ $currentDate = new DateTime();
 
 $time_start = $currentDate->format('Y-m-d-H.i.s') . '.' .  microtime_float();
 
-$file = fopen("/tmp/email_messages/email_{$time_start}.txt", "a");
+$file = fopen("/opt/email_messages/email_{$time_start}.txt", "a");
 fwrite($file, "Script successfully ran at ".date("Y-m-d H:i:s")."\n");
 
 $fd = fopen("php://stdin", "r");
